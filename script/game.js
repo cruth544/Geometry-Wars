8600890f47ce4286399b49bd386bfd669dbd2044
Chad Rutherford <c.ruth544@gmail.com>
2015-11-30 10:22:31 -0800

    adding to gh-pages

--- script/game.js
+++ script/game.js
@@ -1,11 +1,20 @@
+var Shape = function (shape) {
+  this.x
+  this.y
+  this.height
+  this.width
+  this.shape = shape
+  this.collision = function (argument) {
+    // body...
+  }
+  this.rotation = null
+}
 var Player = function (shape) {
-  this.shape
-  this.x;
-  this.y;
-  this.height;
-  this.width;
-  this.rotation;
-  this.collision;
-  this.gun;
-  this.shield;
+  this.gun
+  this.shield
+  this.lives
+  this.score
+}
+var Enemy = function (argument) {
+  this.value
 }
