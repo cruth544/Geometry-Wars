2d6a1b9487a52ac336a737e6fde9d29e7a27072e
Chad Rutherford <c.ruth544@gmail.com>
2015-11-30 11:52:48 -0800

    added keyevent listeners, still working on objects

--- script/engine.js
+++ script/engine.js
@@ -1,6 +1,50 @@
-$function () {
-  function draw (argument) {
 
+  function drawElements (argument) {
+    drawThis.shape(shape1)
   }
-  setInterval(draw(), 1000 / 60)
+
+
+////////////////////////////KEY LISTENERS///////////////////////////////
+$(document).keydown(function(e) {
+  keyListener.down(e)
+})
+$(document).keyup(function(e) {
+  keyListener.up(e)
+})
+
+var keyListener = (function () {
+  var keys = {}
+
+  return {
+    down: function (e) {
+      keys[e.which] = true
+      e.preventDefault()
+      printKeys()
+    },
+    up: function (e) {
+      delete keys[e.which]
+      printKeys()
+    },
+    keyList: function () {
+      return keys
+    }
+  }
+})()
+
+// var keyMap = []
+// onkeydown = onkeyup = function (e) {
+//   e = e || event
+//   keyMap[e.keyCode] = e.type == 'keydown'
+// }
+
+function printKeys() {
+    var html = '';
+    for (var i in keyListener.keyList()) {
+        if (!keyListener.keyList().hasOwnProperty(i)) continue;
+        html += ' ' + i;
+    }
+    $('#out').html(html);
 }
+
+
+setInterval(drawElements, 1000 / 60)
--- script/game.js
+++ script/game.js
@@ -1,20 +1,125 @@
-var Shape = function (shape) {
-  this.x
-  this.y
-  this.height
-  this.width
-  this.shape = shape
-  this.collision = function (argument) {
-    // body...
+var canvas  = $('#game')[0]
+var ctx     = canvas.getContext('2d')
+
+var Characters = (function () {
+  return {
+    Shape: function (shape) {
+      this.x
+      this.y
+      this.height
+      this.width
+      this.shape = null
+      this.color
+      this.collision = function () {
+        // body...
+      }
+      this.rotation = null
+    },
+    Player: function () {
+      this.gun
+      this.shield
+      this.lives
+      this.score
+    },
+    Enemy: function () {
+      this.value
+    }
   }
-  this.rotation = null
+})()
+
+var onGameBoard = (function () {
+  var activeCharacters = []
+
+  return {
+    getIndexForChar: function (character) {
+      return activeCharacters.indexOf(character)
+    },
+    addCharacter: function (character) {
+      var index = this.getIndexForChar(character)
+      if (index > 0) {
+        activeCharacters.push(characters)
+      }
+    },
+    removeCharacter: function (character) {
+      var index = this.getIndexForChar(character)
+      if (index > 0) {
+        activeCharacters.splice(index, 1)
+      }
+    }
+  }
+})()
+
+function startGame (players) {
+
 }
-var Player = function (shape) {
-  this.gun
-  this.shield
-  this.lives
-  this.score
-}
-var Enemy = function (argument) {
-  this.value
-}
+
+var shape1 = new Characters.Shape()
+shape1.x = canvas.width / 2
+shape1.y = canvas.height / 2
+shape1.height = 10
+shape1.width = 10
+shape1.color = '#0095DD'
+
+var drawThis = (function () {
+  return {
+    shape: function (shape) {
+      ctx.beginPath()
+      ctx.rect(shape.x, shape.y, shape.width, shape.height)
+      ctx.fillStyle = shape.color
+      ctx.fill()
+      ctx.closePath()
+    }
+  }
+})()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
